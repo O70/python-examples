f8b2f9e11f5d40994de8b3e7b88522f1eddf9595
鬼王 <THRAEX@aliyun.com>
2019-06-26 08:42:00 +0800

    feat: get tssbs's paragraphs

--- web_crawler/fetch_tssbs.py
+++ web_crawler/fetch_tssbs.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from fetch_util import FetchUtil
+from bs4 import element
 
 class FetchTssbs(FetchUtil):
 	def __init__(self, url):
 		super(FetchTssbs, self).__init__(url)
 		self.__data = {
 			'title': '唐诗三百首',
-			'abstract': '',
+			# 'abstract': '',
 			'content': []
 		}		
 
@@ -27,13 +28,26 @@ class FetchTssbs(FetchUtil):
 			content_list = []
 			for s in tc.find_all('span'):
 				ta = s.text
-				author = None
-				if ta.find('(') > -1:
-					author = ta[ta.find('(') + 1:len(ta) - 1]
+
+				sub_soup = self.get_soup(s.find('a').attrs['href'])
+				cont = (sub_soup.find('div', { 'class': 'main3' }).find('div', { 'class': 'left' })
+					.find('div', { 'class': 'sons' }).find('div', { 'class': 'cont' }))
+				author = cont.find('p', { 'class': 'source' }).get_text()
+
+				paragraphs_list = []
+				for p in cont.find('div', { 'class': 'contson' }).children:
+					if isinstance(p, element.NavigableString):
+						paragraphs_list.append(p.string.strip())
+
+				chapter = s.find('a').string.strip()
+				sc = cont.find('h1').string.strip()
+				if chapter != sc:
+					chapter = '%s [%s]' % (chapter, sc)
+
 				content_list.append({
-					'chapter': s.find('a').string,
+					'chapter': chapter,
 					'author': author,
-					'paragraphs': []
+					'paragraphs': paragraphs_list
 				})
 
 			self.logging.info('%s 共%d首' % (ml.string, len(content_list)))
