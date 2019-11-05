d9287c8cb2ba2c620e0c513c9f1bf472daef8c4e
鬼王 <THRAEX@aliyun.com>
2019-11-05 09:06:00 +0800

    save: source data

--- supermarket/supermarket.py
+++ supermarket/supermarket.py
@@ -2,31 +2,7 @@
 
 #  pip install pymysql --proxy http://proxy1.xx.xx:8080 -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
 
-import sys, json, pymysql
-
-# env = sys.argv[1:]
-# if env:
-# 	env = env[0]
-# else:
-# 	env = 'test'
-
-# config = None
-# with open('configs/config-' + env + '.json', encoding = 'utf-8') as f:
-# 	config = json.load(f)
-
-# connect = pymysql.connect(**config['jdbc'][0])
-
-# cursor = connect.cursor()
-
-# cursor.execute("SELECT id, name FROM base_dict WHERE PARENT_ID = \
-# 	(SELECT id FROM base_dict WHERE code = 'SPLB') ORDER BY level_code")
-
-# categorys = {}
-# for row in cursor.fetchall():
-# 	categorys[row[1]] = row[0]
-# print(categorys)
-
-# connect.close()
+import sys, json, xlrd, pinyin, pymysql
 
 class Supermarket(object):
 	def __init__(self, env):
@@ -37,10 +13,15 @@ class Supermarket(object):
 			self.action = config['action']
 
 	def run(self):
-		pass
+		self.load()
+		# print(self.categorys())
 
 	def load(self):
-		pass
+		wb = xlrd.open_workbook('sources/data-2019.10.31.xlsx')
+		for si in range(wb.nsheets):
+			sh = wb.sheet_by_index(si)
+			# jiushui, liangyou, niunai, roudan, sushi
+			print(sh.name, len(sh.name), pinyin.get(sh.name, format = 'strip'))
 
 	def categorys(self):
 		connect = pymysql.connect(**self.jdbc[0])
