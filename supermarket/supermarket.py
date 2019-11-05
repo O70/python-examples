877c4e539f964d91dc747c8487350940e7ab52ad
鬼王 <THRAEX@aliyun.com>
2019-11-05 11:07:21 +0800

    feat: data assembly

--- supermarket/supermarket.py
+++ supermarket/supermarket.py
@@ -2,7 +2,7 @@
 
 #  pip install pymysql --proxy http://proxy1.xx.xx:8080 -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
 
-import sys, os, json, xlrd, pinyin, pymysql
+import sys, os, json, xlrd, pinyin, uuid, datetime, pymysql
 
 class Supermarket(object):
 	def __init__(self, env):
@@ -14,13 +14,13 @@ class Supermarket(object):
 
 	def run(self):
 		imgs = self.loadImg()
-		self.loadData(imgs)
-
-		# self.load()
-		# print(self.categorys())
+		categorys = self.categorys()
+		rows = self.loadData(imgs, categorys)
 
 	def loadImg(self):
 		# jiushui, liangyou, niunai, roudan, sushi
+
+		print('Start loading images...')
 
 		results = {}
 		dirpath = 'sources/imgs/'
@@ -30,28 +30,69 @@ class Supermarket(object):
 		for img in imgs:
 			results[img.split('_')[0]] = '%s%s' % (dirpath, img)
 
+		print('End of image loading.')
+
 		return results
 
-	def loadData(self, imgs):
+	def loadData(self, imgs, categorys):
+		print('Assembly data begins...')
+
+		results = []
+
 		wb = xlrd.open_workbook('sources/data-2019.10.31.xlsx')
 		total = 0
 		for si in range(wb.nsheets):
 			sh = wb.sheet_by_index(si)
 			rows = sh.nrows - 2
 			total += rows
-			print('*********************** %s: %d rows ***********************' % (sh.name, rows))
+
+			category = categorys[sh.name]
+			prefix = pinyin.get_initial(sh.name, '').upper()
+
+			print('************* %s: %drows *************' % (sh.name, rows))
 			for ri in range(2, sh.nrows):
 				ind = int(sh.cell_value(ri, 0))
 				name = sh.cell_value(ri, 1).strip()
+				unit = sh.cell_value(ri, 2).strip()
+
+				price = sh.cell_value(ri, 3)
+				ptype = sh.row(ri)[3].ctype
+				if ptype == 1:
+					price = price.strip()
+					yind = price.find('元')
+					if yind != -1:
+						price = float(price[:yind])
+					else:
+						price = 0.0
+				elif ptype == 0:
+					price = 0.0
+
 				imgpath = None
 				try:
 					imgpath = imgs['%s%d' % (pinyin.get(sh.name[:2], format = 'strip'), ind)]
 				except Exception as e:
-					print('Image not found: %s' % name)
+					print('Image not found: %s(%s)' % (name, e))
 
-				print(name, imgpath)
+				row = {
+					'id': str(uuid.uuid1()).replace('-', ''),
+					'name': name,
+					'numeration': '%s%06d' % (prefix, ind),
+					'img': imgpath,
+					'price': price,
+					'unit': unit,
+					'specs': '%.2f/%s' % (price, unit),
+					'amount': 0,
+					'category': category,
+					'enabled': 1,
+					'create_by': 'admin',
+					'create_time': datetime.datetime.now()
+				}
+				# print(row)
+				results.append(row)
 
-		print('Total: %d rows.' % total)
+		print('End of assembly data. Total: %d rows.' % total)
+
+		return results
 
 	def categorys(self):
 		connect = pymysql.connect(**self.jdbc[0])
