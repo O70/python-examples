e8b6e73892b21f24f3ec5abec62850fc3e3bfdd0
鬼王 <THRAEX@aliyun.com>
2019-11-18 16:11:05 +0800

    chore: update config

--- gitlab/users.py
+++ gitlab/users.py
@@ -1,72 +1,64 @@
 # -*- coding: utf-8 -*-
 
-import requests, json
-
-# private_token
-
-# 70 root
-HEADERS = { 'PRIVATE-TOKEN': 'TEoi3qgzkem7E4Gx4JE1' }
-# 70 Guiwang
-# HEADERS = { 'PRIVATE-TOKEN': 'yutBj4yFUJKDdKs7kKxZ' }
-
-# 77 root
-# HEADERS = { 'PRIVATE-TOKEN': '9abAzt2BGy_-T5ev_BW8' }
-
-# 50 root
-# HEADERS = { 'PRIVATE-TOKEN': '9ysgzsRULvzMEYdB5p_g' }
+import sys, requests, json, config
 
 class Users(object):
-	def __init__(self):
+	def __init__(self, env = 'test'):
 		super(Users, self).__init__()
+		self.api = 'http://%s/api/v4/users'
+		self.source = config.SOURCE
+		self.target = config.TARGET.get(env, config.TARGET['test'])
 
 	def run(self):
-		# sort/order_by当前版本不提供
-		# params = {
-		# 	'sort': 'asc',
-		# 	'order_by': 'id',
-		# 	'per_page': 50
-		# }
-		url = 'http://10.27.213.70/api/v4/users'
-		# url = 'http://10.122.163.77/api/v4/users'
-		# url = 'http://11.11.141.50/api/v4/users'
-		resp = requests.get(url, headers = HEADERS, params = { 'per_page': 50 })
+		users = self.get()
+		self.inserts(users)
+
+	def get(self):
+		resp = requests.get(
+			self.api % self.source['address'], 
+			headers = { 'PRIVATE-TOKEN': self.source['access_token'] }, 
+			params = { 'per_page': 50 })
 
 		users = sorted(resp.json(), key = lambda x:x['id'], reverse = False)
-
-		# for user in users:
-		# 	print(user.get('id'), user.get('username'), user.get('name'))
-
-		print('Total: ', len(users))
-
 		with open('tmp/users.json', 'w', encoding = 'UTF-8') as f:
 			json.dump(users, f, sort_keys = False, indent = 2, ensure_ascii = False)
 
-		gw = users[1]
-		print((gw.get('is_admin')))
-		print(type(gw.get('is_admin')))
-		data = {
-			'email': gw.get('email'),
-			'password': '11111111',
-			'username': gw.get('username'),
-			'name': gw.get('name'),
-			'skype': gw.get('skype'),
-			'linkedin ': gw.get('linkedin '),
-			'twitter': gw.get('twitter'),
-			'website_url': gw.get('website_url'),
-			'organization': gw.get('organization'),
-			'bio': gw.get('bio'),
-			'location': gw.get('location'),
-			'admin': gw.get('is_admin'),
-			'skip_confirmation': True
-		}
+		print('Total accounts: %d' % len(users))
 
-		resp2 = requests.delete('http://10.122.163.77/api/v4/users/12?hard_delete=true',
-			headers = { 'PRIVATE-TOKEN': '9abAzt2BGy_-T5ev_BW8' })
-		print(resp2.text)
+		return users
 
-		# resp1 = requests.post('http://10.122.163.77/api/v4/users', 
-		# 	headers = { 'PRIVATE-TOKEN': '9abAzt2BGy_-T5ev_BW8' }, data = data)
-		# print(resp1.text)
+	def inserts(self, users):
+		c = 0
+		for user in users:
+			uname = user['username']
+			if uname != 'root' and uname != 'ghost':
+				data = {
+					'email': user.get('email'),
+					'password': '11111111',
+					'username': user.get('username'),
+					'name': user.get('name'),
+					'skype': user.get('skype'),
+					'linkedin ': user.get('linkedin '),
+					'twitter': user.get('twitter'),
+					'website_url': user.get('website_url'),
+					'organization': user.get('organization'),
+					'bio': user.get('bio'),
+					'location': user.get('location'),
+					'admin': user.get('is_admin'),
+					'skip_confirmation': True
+				}
+				requests.post(
+					self.api % self.target['address'], 
+					headers = { 'PRIVATE-TOKEN': self.target['access_token'] }, 
+					data = data)
+				c += 1
+		print('Create new user: %d' % c)
 
 if __name__ == '__main__':
-	Users().run()
+	env = sys.argv[1:]
+	if env:
+		env = env[0]
+	else:
+		env = 'test'
+
+	Users(env).run()
