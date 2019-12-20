192974bb8a8c277b4f37edc54ce7fd558cc5c7c9
鬼王 <THRAEX@aliyun.com>
2019-12-20 15:34:08 +0800

    feat: get wzmq v2

--- web_crawler/fetch_wzmq_v2.py
+++ web_crawler/fetch_wzmq_v2.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from fetch_util import FetchUtil
+from bs4 import BeautifulSoup, element
 import re
 
 class FetchWzmq(FetchUtil):
-	def __init__(self, url):
+	def __init__(self, url = None):
 		super(FetchWzmq, self).__init__(url)
 		self.__data = {
 			'title': '文字蒙求',
@@ -13,16 +14,42 @@ class FetchWzmq(FetchUtil):
 		}
 
 	def processing(self):
-		soup = self.get_soup(self.url)
-		print(soup)
+		# soup = self.get_soup(self.url)
+		with open('files/wiki_wenzimengqiu.html') as f:
+			soup = BeautifulSoup(f.read(), "html.parser")
 		self.set_content(soup)
 		self.to_json('./jsons/wenzimengqiu.json', self.__data)
 
 	def set_content(self, soup):
 		soup_tr = soup.find_all('tr', id = re.compile('^p[0-9]+'))
-		# print(len(soup_tr))
+		total = len(soup_tr)
+		print('Total tr:', total)
 
-		# for tr in soup_tr:
-		# 	print(tr.name, tr['class'])
+		alen = 2
+		for i in range(alen):
+			for x in soup_tr[i].contents[1].children:
+				if x.string:
+					self.__data['abstract'].append(x.string)
 
-FetchWzmq('https://ctext.org/wiki.pl?if=gb&chapter=912588&remap=gb').processing()
+		for i in range(alen, total):
+			tr = soup_tr[i]
+			sub_title = tr.find(colspan = 2)
+			if sub_title:
+				self.__data['content'].append({
+					'title': sub_title.contents[0]['id'].replace(self.__data['title'], ''),
+					'paragraphs': []
+				})
+			else:
+				content = self.__data['content']
+				para = content[len(content) - 1]['paragraphs']
+				td = tr.contents[1]
+				for x in td.children:
+					if isinstance(x, element.NavigableString):
+						para.append(x.string)
+					else:
+						txt = x.get_text()
+						if txt:
+							para.append(txt)
+
+# FetchWzmq('https://ctext.org/wiki.pl?if=gb&chapter=912588&remap=gb').processing()
+FetchWzmq().processing()
