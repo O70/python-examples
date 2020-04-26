cfda7bb43d199377c4cff5788d8a34c969e953d7
鬼王 <THRAEX@aliyun.com>
2020-04-26 18:29:05 +0800

    test: nlp

---
+++ nlp/text_classification.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import jieba
+import json
+
+source_path = 'tmp/baike_qa_train.json'
+target_path = 'tmp/train_data.json'
+
+def main():
+	with open(source_path, encoding = 'utf-8') as f:
+		print(f.readlines()
+
+if __name__ == '__main__':
+	main()
